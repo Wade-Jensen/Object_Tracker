95f0caed15ae617f4ec15e22d5393f7187ecbad7
salehalbarnawi <s.m.m.b.8@hotmail.com>
2016-06-02 07:29:25 +0500

    Add files via upload

---
+++ include/inputParams.H
@@ -0,0 +1,41 @@
+#ifndef INPUTPARAMS_H
+#define INPUTPARAMS_H
+
+#include <vul/vul_file_iterator.h>
+
+#ifndef _VUL_FILE_
+#define _VUL_FILE_
+#include <vul/vul_file.h>
+#endif
+#ifndef _VIL_LOAD_
+#define _VIL_LOAD_
+#include <vil/vil_load.h>
+#endif
+#include "vcl_vector.h"
+#include "vcl_string.h"
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+class inputParams
+{
+    public:
+        /** Constructor */
+        inputParams();
+        bool initInputParams(vcl_vector<vcl_string>, int, int, float, float);
+        bool textParamsReader();
+
+        /** Destructor */
+        ~inputParams();
+        /** Parameters */
+        vcl_vector<vcl_string> filenames;
+        int ipx, ipy;
+        float sd,lr;
+    protected:
+
+
+    private:
+};
+
+#endif // INPUTPARAMS_H
