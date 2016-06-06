53eaf25415ecde4fa8ed7a680a7df0df505bfabd
LachlanNXT <lachlan-27@hotmail.com>
2016-06-06 22:11:09 +1000

    batch

---
+++ bin/Release/goodBat.bat
@@ -0,0 +1,10 @@
+REM correct arguments start here
+ObjectTracker.exe -path ../../Data/bolt -glob .jpg -ipx 336 -ipy 165 -w 25 -h 60 -c 8 -sb 4 -bc 1 -sd 30 -planes 3 -extend 1 -colour 1 -lr 0.05 -sds 1.0 -sdc 0.625 -odir Output/outputBolt
+
+ObjectTracker.exe -path ../../Data/bicycle -glob .jpg -ipx 154 -ipy 94 -w 18 -h 48 -c 8 -sb 4 -bc 1 -sd 30 -planes 3 -extend 1 -colour 1 -lr 0.05 -sds 1.0 -sdc 0.625 -odir Output/outputBicycle
+
+ObjectTracker.exe -path ../../Data/cup -glob .jpg -ipx 125 -ipy 92 -w 47 -h 59 -c 8 -sb 4 -bc 1 -sd 30 -planes 3 -extend 1 -colour 1 -lr 0.05 -sds 1.0 -sdc 0.625 -odir Output/outputCup
+
+ObjectTracker.exe -path ../../Data/juice -glob .jpg -ipx 131 -ipy 50 -w 37 -h 90 -c 8 -sb 4 -bc 1 -sd 30 -planes 3 -extend 1 -colour 1 -lr 0.05 -sds 1.0 -sdc 0.625 -odir Output/outputJuice
+
+ObjectTracker.exe -path ../../Data/jump -glob .jpg -ipx 328 -ipy 189 -w 53 -h 50 -c 8 -sb 4 -bc 1 -sd 30 -planes 3 -extend 1 -colour 1 -lr 0.05 -sds 1.0 -sdc 0.625 -odir Output/outputJump
